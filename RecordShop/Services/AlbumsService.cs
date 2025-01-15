29ab5ee3a17f32904029ebd34a04e9576cc1296c
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-15 09:42:40 +0000

    Feat: Add private method to create AlbumDTO in service

--- RecordShop/Services/AlbumsService.cs
+++ RecordShop/Services/AlbumsService.cs
@@ -18,18 +18,40 @@ namespace RecordShop.Services
         {
             List<AlbumDTO> albumInfo = _albumsRepository.GetAllAlbums()
                                                      .ToList()
-                                                     .Select(a => new AlbumDTO
-                                                     {
-                                                         Id = a.Id,
-                                                         Title = a.Title,
-                                                         ArtistName = a.AlbumArtist?.Name ?? "Unknown",
-                                                         MusicGenre = a.MusicGenre.ToString(),
-                                                         ReleaseYear = a.ReleaseYear,
-                                                         Stock = a.Stock
-                                                     })
+                                                     .Select(a => createAlbumDTO(a))
                                                      .ToList();
             return albumInfo;
                                                      
+        }
+        public Album GetAlbumByID(int id)
+        {
+            return null;
+        }
+
+        public Album AddAlbum(AlbumDTO album)
+        {
+            return null;
+        }
+        public Album UpdateAlbum(int id)
+        {
+            return null;
+        }
+        public void DeleteAlbum(int id)
+        {
+          
+        }
+        private AlbumDTO createAlbumDTO(Album album)
+        {
+            AlbumDTO albumDTO = new AlbumDTO()
+            {
+                Id = album.Id,
+                Title = album.Title,
+                ArtistName = album.AlbumArtist?.Name ?? "Unknown",
+                MusicGenre = album.MusicGenre.ToString(),
+                ReleaseYear = album.ReleaseYear,
+                Stock = album.Stock
+            };
+            return albumDTO;
         }
 
     }
