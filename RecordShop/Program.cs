dde758b848107fc7969bd3c34a5969251baa1f89
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-02-24 23:55:33 +0000

    Update to switch Database for Prod and Dev Environment

--- RecordShop/Program.cs
+++ RecordShop/Program.cs
@@ -5,21 +5,22 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//if (builder.Environment.IsDevelopment())
-//{
-//    builder.Services.AddDbContext<RecordShopDbContext>(options =>
-//                            options.UseInMemoryDatabase(databaseName: "RecordShopDB"));
-//}
-//else if (builder.Environment.IsProduction())
-//{
-//var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<RecordShopDbContext>(options =>
+                            options.UseInMemoryDatabase(databaseName: "RecordShopDB"));
+}
+else if (builder.Environment.IsProduction())
+{
+    var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
-//string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = "Server=.\\SQLEXPRESS;Database=RecordShopDB;User Id=sa;Password=Password@123;Trust Server Certificate=True";
+    //string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    //var connectionString = "Server=.\\SQLEXPRESS;Database=RecordShopDB;User Id=sa;Password=Password@123;Trust Server Certificate=True";
     builder.Services.AddDbContext<RecordShopDbContext>(options =>
                            options.UseSqlServer(connectionString));
+    Console.WriteLine("Connection String : " + connectionString);
 
-
+}
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IAlbumsRepository, AlbumsRepository>();
@@ -34,17 +35,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//    // Ensures the in-memory database is initialized
-//    using (var scope = app.Services.CreateScope())
-//    {
-//        var dbContext = scope.ServiceProvider.GetRequiredService<RecordShopDbContext>();
-//        dbContext.Database.EnsureCreated();
-//    }
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // Ensures the in-memory database is initialized
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<RecordShopDbContext>();
+        dbContext.Database.EnsureCreated();
+    }
+}
 
 app.UseHttpsRedirection();
 
